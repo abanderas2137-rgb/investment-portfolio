23a5941d3f55baf6a66633d9897a53d6d6c20a65
abanderas2137-rgb <abanderas2137@gmail.com>
2026-09-21 12:09:20 +0200

    Extend portfolio models for market data and analytics

--- Portfolio.cs
+++ Portfolio.cs
@@ -2,5 +2,10 @@ using System.Collections.ObjectModel;
 using InvestmentPortfolio.Models;
 namespace InvestmentPortfolio
 {
- public class Portfolio { public ObservableCollection<Investment> Investments { get; } = new ObservableCollection<Investment>(); }
+ public class Portfolio
+ {
+  public ObservableCollection<Investment> Investments { get; } = new ObservableCollection<Investment>();
+  public ObservableCollection<PortfolioTransaction> Transactions { get; } = new ObservableCollection<PortfolioTransaction>();
+  public ObservableCollection<PortfolioSnapshot> Snapshots { get; } = new ObservableCollection<PortfolioSnapshot>();
+ }
 }
