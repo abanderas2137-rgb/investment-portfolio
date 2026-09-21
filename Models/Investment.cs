f93f5d55f1c329b4481284077b1ca27d3863b3c3
abanderas2137-rgb <abanderas2137@gmail.com>
2026-09-21 11:23:08 +0200

    Add investment model

---
+++ Models/Investment.cs
@@ -0,0 +1,21 @@
+using System;
+namespace InvestmentPortfolio.Models
+{
+ public enum InvestmentType { Stock, Bond, ETF, Deposit, Cryptocurrency, Fund, Other }
+ public class Investment
+ {
+  public Guid Id { get; set; } = Guid.NewGuid();
+  public string Name { get; set; }
+  public string Ticker { get; set; }
+  public InvestmentType Type { get; set; }
+  public string Currency { get; set; }
+  public decimal Quantity { get; set; }
+  public decimal PurchasePrice { get; set; }
+  public decimal CurrentPrice { get; set; }
+  public DateTime PurchaseDate { get; set; }
+  public decimal InvestedValue => Quantity * PurchasePrice;
+  public decimal CurrentValue => Quantity * CurrentPrice;
+  public decimal ProfitLoss => CurrentValue - InvestedValue;
+  public decimal ProfitLossPercent => InvestedValue == 0 ? 0 : ProfitLoss / InvestedValue * 100;
+ }
+}
